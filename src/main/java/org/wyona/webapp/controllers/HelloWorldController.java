2ba89c8a315dd89a62099c3389336a770bfe6e0a
Michael Wechner <michael.wechner@wyona.com>
2018-06-08 18:00:32 +0200

    annotations added

--- src/main/java/org/wyona/webapp/controllers/HelloWorldController.java
+++ src/main/java/org/wyona/webapp/controllers/HelloWorldController.java
@@ -8,6 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 import org.wyona.webapp.models.Greeting;
 
@@ -15,7 +19,7 @@ import org.wyona.webapp.models.Greeting;
  * 'Hello World' Controller 
  */
 @RestController
-@RequestMapping(value = "/api/greeting") 
+@RequestMapping(value = "/api/greeting")
 public class HelloWorldController {
 
     private static final Logger logger = LogManager.getLogger("HelloWorldController");
@@ -24,7 +28,10 @@ public class HelloWorldController {
      *
      */
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<Greeting> getGreeting() {
+    @ApiOperation(value="Generate greeting and send greeting as email when address specified") 
+    public ResponseEntity<Greeting> getGreeting(
+        @ApiParam(name = "email", value = "email address greeting will be sent to, e.g. 'michael.wechner@wyona.com'", required = false) @RequestParam(name = "email", required = false) String email
+        ) {
         logger.info(new Greeting().getGreeting());
         return new ResponseEntity<>(new Greeting(), HttpStatus.OK);
     }
