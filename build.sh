4318245965221beab8f04fed5b7cea85afc59e74
Michael Wechner <michael.wechner@wyona.com>
2018-10-06 21:07:17 +0200

    tomcat check moved

--- build.sh
+++ build.sh
@@ -2,17 +2,16 @@
 
 TOMCAT_HOME=/Users/michaelwechner/local/apache-tomcat-8.0.26
 
-if [ ! -d $TOMCAT_HOME ];then
-    echo "ERROR: No Tomcat installed at $TOMCAT_HOME"
-    exit 0
-fi
-
 echo "INFO: Build 'Hello World' webapp ..."
 mvn clean install -Dmaven.test.skip=true
 #mvn clean install
 #mvn -X clean install
 
-echo "INFO: Deploy webapp ..."
+echo "INFO: Trying to deploy webapp ..."
+if [ ! -d $TOMCAT_HOME ];then
+    echo "ERROR: No Tomcat installed at $TOMCAT_HOME"
+    exit 0
+fi
 rm -rf $TOMCAT_HOME/webapps/hello-world-webapp-*
 rm -rf $TOMCAT_HOME/work/Catalina/localhost/hello-world-webapp-*
 cp target/hello-world-webapp-1.0.0-SNAPSHOT.war $TOMCAT_HOME/webapps/.
