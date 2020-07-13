d158af975fbdb3a8e6d3b12d29f6120d6115d68f
Michael Wechner <michael.wechner@wyona.com>
2020-07-13 09:17:07 +0200

    obsolete  code removed

--- src/main/java/org/wyona/webapp/models/LanguageEmail.java
+++ src/main/java/org/wyona/webapp/models/LanguageEmail.java
@@ -1,55 +1,19 @@
 package org.wyona.webapp.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.wyona.webapp.interfaces.EmailValidation;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LanguageEmail implements Serializable {
-
-    public enum Language {
-
-        ENGLISH("en", "Hello World"),
-        GERMAN("de", "Hallo Welt"),
-        BOSNIAN("ba", "Pozdrav svijetu");
-
-        private final String languageCode;
-        private final String message;
-
-        Language(String languageCode, String message) {
-            this.languageCode = languageCode;
-            this.message = message;
-        }
-
-        public static Language getLanguageByCode(String languageCode) {
-            for (Language l: values()) {
-                if (l.languageCode.equals(languageCode))
-                    return l;
-            }
-
-            // avoid returning null and then checking in caller whether the returned value is null. Try to localize the logic.
-            throw new IllegalArgumentException("Language '" + languageCode + "' not supported yet!");
-        }
-
-        public String getMessage() {
-            return this.message;
-        }
-    }
-
     @NotBlank(message = "Email cannot be empty or null")
     @Email(regexp = EmailValidation.EMAIL_VALIDATION_REGEX, message = "Submitted email is not valid")
     private String email;
-
-    @NotBlank(message = "Language code cannot be empty or null")
-    private String languageCode;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getLanguageCode() {
-        return languageCode;
-    }
-
 }
