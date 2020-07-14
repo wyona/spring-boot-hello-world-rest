3cd34e69beff7f6abd6ecde51b8935ca6d8033e6
Michael Wechner <michael.wechner@wyona.com>
2020-07-14 22:42:14 +0200

    log statement added

--- src/main/java/org/wyona/webapp/config/ThreadConfig.java
+++ src/main/java/org/wyona/webapp/config/ThreadConfig.java
@@ -6,13 +6,16 @@ import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 @EnableAsync
 public class ThreadConfig {
 	
-	@Bean
+    @Bean
     public TaskExecutor threadPoolTaskExecutor() {
- 
+        log.info("Configuring task executor ..."); 
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(4);
         executor.setMaxPoolSize(4);
@@ -21,5 +24,4 @@ public class ThreadConfig {
  
         return executor;
     }
-	
 }
