31afed0a89ed3a2c6ff18eeaceb6f50b1aac2301
Michael Wechner <michael.wechner@wyona.com>
2022-02-05 00:08:13 +0100

    Weaviate implementation started

--- src/main/java/org/wyona/webapp/controllers/v2/KatieMockupConnectorController.java
+++ src/main/java/org/wyona/webapp/controllers/v2/KatieMockupConnectorController.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import org.wyona.webapp.models.LanguageEmail;
-
 import lombok.extern.slf4j.Slf4j;
 import org.wyona.webapp.models.katie.Domain;
 import org.wyona.webapp.models.katie.QnA;
+
+import technology.semi.weaviate.client.Config;
+import technology.semi.weaviate.client.WeaviateClient;
+import technology.semi.weaviate.client.v1.data.model.WeaviateObject;
+import technology.semi.weaviate.client.base.Result;
+import technology.semi.weaviate.client.v1.misc.model.Meta;
 
 /**
  * 'Katie Mockup Connector' Controller
@@ -21,6 +25,9 @@ import org.wyona.webapp.models.katie.QnA;
 @RequestMapping(value = "/api/v2")
 @AllArgsConstructor
 public class KatieMockupConnectorController implements KatieConnectorController {
+
+    private static final String weaviateHost = "localhost:8080";
+    private static final String weaviateProtocol = "http";
 
     /**
      * @see org.wyona.webapp.controllers.v2.KatieConnectorController#createTenant(Domain)
@@ -61,7 +68,36 @@ public class KatieMockupConnectorController implements KatieConnectorController 
      *
      */
     private ResponseEntity<String> createTenantWeaviateImpl(Domain domain) {
-        log.info("TODO: Weaviate Impl: Create tenant associated with Katie domain ID '" + domain.getId() + "' ...");
+        log.info("Weaviate Impl: Create tenant associated with Katie domain ID '" + domain.getId() + "' ...");
+        Config config = new Config(weaviateProtocol, weaviateHost);
+        WeaviateClient client = new WeaviateClient(config);
+
+        java.util.Map<String, Object> properties = new java.util.HashMap<>();
+        properties.put("name", domain.getName());
+
+        Result<WeaviateObject> result = client.data().creator()
+                .withClassName("Tenant")
+                .withID(domain.getId())
+                .withProperties(properties)
+                .run();
+
+        if (result.hasErrors()) {
+            log.error("" + result.getError().getMessages());
+        } else {
+            log.info("" + result.getResult());
+        }
+
+        /*
+        Result<Meta> meta = client.misc().metaGetter().run();
+        if (meta.getError() == null) {
+            log.info("meta.hostname: " + meta.getResult().getHostname());
+            log.info("meta.version: " + meta.getResult().getVersion());
+            log.info("meta.modules: " + meta.getResult().getModules());
+        } else {
+            log.error("" + meta.getError().getMessages());
+        }
+         */
+
         return new ResponseEntity<>("{}", HttpStatus.OK);
     }
 }
