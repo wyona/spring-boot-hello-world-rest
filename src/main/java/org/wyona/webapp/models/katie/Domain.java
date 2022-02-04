3fbba7e61fe4d9ff497f0fdddbc4de0e86492397
Michael Wechner <michael.wechner@wyona.com>
2022-02-04 13:45:16 +0100

    domain model added

---
+++ src/main/java/org/wyona/webapp/models/katie/Domain.java
@@ -0,0 +1,30 @@
+package org.wyona.webapp.models.katie;
+
+/**
+ *
+ */
+public class Domain {
+
+    private String id;
+    private String name;
+
+    /**
+     *
+     */
+    public Domain() {
+    }
+
+    /**
+     *
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     */
+    public String getId() {
+        return id;
+    }
+}
