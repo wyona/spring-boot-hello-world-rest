17bfa8dd85f070277bcc58d5dfe372dba95d411f
Michael Wechner <michael.wechner@wyona.com>
2022-02-04 13:51:32 +0100

    name added and javadoc improved

--- src/main/java/org/wyona/webapp/models/katie/Domain.java
+++ src/main/java/org/wyona/webapp/models/katie/Domain.java
@@ -15,14 +15,28 @@ public class Domain {
     }
 
     /**
-     *
+     * @param name Katie domain name, e.g. "Wyona FAQ"
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return Katie domain name, e.g. "Wyona FAQ"
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param id Katie domain Id, e.g. "01b40185-ea12-4ae0-8df7-b335ad0d8817"
      */
     public void setId(String id) {
         this.id = id;
     }
 
     /**
-     *
+     * @return Katie domain Id, e.g. "01b40185-ea12-4ae0-8df7-b335ad0d8817"
      */
     public String getId() {
         return id;
